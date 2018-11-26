316b1197ad4d60ebda444830842f4424ea10e625
JaniceNova <janicenovacek2016@gmail.com>
2018-11-25 20:30:41 -0800

    push info to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
+var config = {
+    apiKey: "AIzaSyDs5xVFHhiKt-odAyiaL4_TkvJ5Kqp0DQE",
+    authDomain: "cbc-demo-1d9a7.firebaseapp.com",
+    databaseURL: "https://cbc-demo-1d9a7.firebaseio.com",
+    projectId: "cbc-demo-1d9a7",
+    storageBucket: "cbc-demo-1d9a7.appspot.com",
+    messagingSenderId: "1069776390766"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+
 $("#Submit").on("click", function () {
     console.log("addTrain")
     var destination = $("#destination-input").val().trim();
@@ -10,5 +24,12 @@ $("#Submit").on("click", function () {
     console.log(time);
 
     $(".train-view").append("<tr class='center'><td>" + train + "</td><td>" + destination + "</td><td>" + mins + "</td><td>" + time + "</td><td>" + "blank" + "</td></tr>")
+
+    database.ref().push({
+        train: train,
+        destination: destination,
+        mins: mins,
+        time: time
+      });
 });
 
