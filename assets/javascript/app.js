2adcd98b26169fc24ee238f1e43fca0a48de18a5
JaniceNova <janicenovacek2016@gmail.com>
2018-11-25 16:55:00 -0800

    set up var for user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,14 @@
-$("#Submit").on("click", function() {
+$("#Submit").on("click", function () {
+    console.log("addTrain")
+    var destination = $("#destination-input").val().trim();
+    var train = $("#train-input").val().trim();
+    var time = $("#time-input").val().trim();
+    var mins = $("#mins-input").val().trim();
+    console.log(destination);
+    console.log(train);
+    console.log(mins);
+    console.log(time);
 
-    console.log("Hello")
 });
+
+// destination-input  train-input time-input  min-input
