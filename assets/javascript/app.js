d0f6a025a8197d784478075cee91dd611f8b6b4a
JaniceNova <janicenovacek2016@gmail.com>
2018-11-26 15:15:53 -0800

    added delete button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,11 +9,12 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-
+var d = new Date();
   var destination = $("#destination-input").val().trim();
     var train = $("#train-input").val().trim();
     var time = $("#time-input").val().trim();
     var mins = $("#mins-input").val().trim();
+    console.log(moment().format("DD/MM/YY hh:mm A"));
 
 $("#Submit").on("click", function () {
     event.preventDefault();
@@ -40,24 +41,39 @@ $("#Submit").on("click", function () {
 
 
 database.ref().on("child_added", function (snapshot) {
-  
+  console.log(snapshot.key)
     console.log(snapshot.val().train)
    console.log(snapshot.val().destination)
    console.log(snapshot.val().mins)
   console.log(snapshot.val().time)
+  var id = snapshot.key;
   
 
-    $(".train-view").append("<tr class='center'><td class='trains'>" + snapshot.val().train + "</td><td class='destination'>" + snapshot.val().destination+ "</td><td class='mins'>" + snapshot.val().mins + "</td><td class='time'>" + snapshot.val().time + "</td><td>" + "blank" + "</td><td ><button class='updatebutton'>Update</button></td><td ><button class='deletebutton'>Delete</button></td></tr>")
+    $(".train-view").append("<tr class='center'><td class='trains'>" + snapshot.val().train + "</td><td class='destination'>" + snapshot.val().destination+ "</td><td class='mins'>" + snapshot.val().mins + "</td><td class='time'>" + snapshot.val().time + "</td><td>" + "blank" + "</td><td ><button class='updatebutton'>Update</button></td><td ><button class='deletebutton' data-id=" + snapshot.key + ">Delete</button></td></tr>")
 
 });
 
-$(document).on("click", ".deletebutton", function () {
-    event.preventDefault();
-    console.log("hi")
+$(document).on("click", ".deletebutton", function (snapshot) {
+//     event.preventDefault();
+     console.log("hi")
+     var buttonID= $(this).attr("data-id")
+     database.ref(buttonID).remove();
+//     console.log(database.ref(snapshot.val().train))
+ 
+ });
+// $(document).on("click", ".updatebutton", function () {
+//     event.preventDefault();
+//     console.log("hi")
     
-});
-$(document).on("click", ".updatebutton", function () {
-    event.preventDefault();
-    console.log("hi")
-    
-});
+// });
+
+// var remove = function(e){
+//     e.preventDefault();
+//     e.stopPropogation();
+//     console.log("hi")
+//     var key = $(this).data(name);
+  
+//       database.ref().child().remove(name);
+  
+//   }
+//   $(document).on('click', '.deletebutton', remove);
